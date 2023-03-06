453fda72e1ac6f48d16f9af9b33187cc9c69fc53
Lauren-Gar <125327501+Lauren-Gar@users.noreply.github.com>
2023-03-06 07:44:40 -0500

    Updated .css

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-console.log("Hi! Welcome to My Portfolio Site!")
+$('#heading-text').text('How to create a hamburger menu with jQuery');
 
 function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
-      x.className += ' responsive';
-    } else {
-      x.className = 'navtoggle';
-    }
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
   }
+}
